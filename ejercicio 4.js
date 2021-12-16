9138e8102a1810f5cf752ae9d784501d8c8d88a0
abigailhdez <95898495+abigailhdez@users.noreply.github.com>
2021-12-15 18:18:09 -0600

    version1

---
+++ ejercicio 4.js
@@ -0,0 +1,36 @@
+
+
+var student1Courses = ['Math', 'English', 'Programming'];
+var student2Courses = ['Geography', 'Spanish', 'Programming'];
+//Create a program that loops over the 2 arrays; if there are any common courses print them out to the console
+
+
+
+if (student1Courses[0] == student2Courses[0]){
+    console.log(student1Courses[0] + " " + student2Courses[0]);
+}
+else if(student1Courses[0] == student2Courses[1]){
+    console.log(student1Courses[0] + " " + student2Courses[1]);
+}
+
+else if(student1Courses[0] == student2Courses[2]){
+    console.log(student1Courses[0] + " " + student2Courses[2])
+}
+else if(student1Courses[1] == student2Courses[0]){
+    console.log(student1Courses[1] + " " + student2Courses[0])
+}
+else if(student1Courses[1] == student2Courses[1]){
+    console.log(student1Courses[1] + " " + student2Courses[1])
+}
+else if(student1Courses[1] == student2Courses[2]){
+    console.log(student1Courses[1] + " " + student2Courses[2])
+}
+else if(student1Courses[2] == student2Courses[0]){
+    console.log(student1Courses[2] + " " + student2Courses[0])
+}
+else if(student1Courses[2] == student2Courses[1]){
+    console.log(student1Courses[2] + " " + student2Courses[1])
+}
+else if(student1Courses[2] == student2Courses[2]){
+    console.log(student1Courses[2] + " " + student2Courses[2])
+}
